b5930ff4b85bfea709b9937e61995710fd2f2c55
dvdg6566 <czhdaniel@gmail.com>
2024-11-02 19:43:42 +0000

    some audio stuff works?

--- audio-processing/main.py
+++ audio-processing/main.py
@@ -1,7 +1,28 @@
 import base64
+import speech_recognition as sr
+import soundfile as sf
+import io as io
+import wave
+import subprocess
 
 with open("test_audio", "r") as f:
     audio_string = f.read()
 
-with open("output.wav", "wb") as f:
-    f.write(base64.b64decode(audio_string))
+print(len(audio_string))
+webm_filename = "output.webm"
+wav_filename = "output.wav"
+r = sr.Recognizer()
+
+with open(webm_filename, "wb") as f:
+    decoded_string = base64.b64decode(audio_string)
+    f.write(decoded_string)
+
+command = f"ffmpeg -i {webm_filename} -vn -acodec pcm_s16le -ar 44100 -ac 2 {wav_filename}"
+subprocess.run(command, shell=True)
+
+with sr.AudioFile(wav_filename) as source:
+    # Listen for the data (load audio to memory)
+    audio_data = r.record(source)
+
+text = r.recognize_google(audio_data)  
+print("Transcription: ", text)
